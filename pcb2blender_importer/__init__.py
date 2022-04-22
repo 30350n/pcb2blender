33b79fffae2ad9f2f8f2eb83229ff830303e9dc2
Bobbe <maxschlecht@web.de>
2022-04-22 17:59:17 +0200

    Add github url, change tabs to spaces in __init__.py

--- pcb2blender_importer/__init__.py
+++ pcb2blender_importer/__init__.py
@@ -1,13 +1,13 @@
 bl_info = {
-	"name": "pcb2blender importer",
-	"description": "Enables Blender to import .pcb3d files, exported from KiCad.",
-	"author": "Bobbe",
-	"version": (1, 0, 0),
-	"blender": (3, 0, 0),
-	"location": "File > Import",
-	"category": "Import-Export",
-	"support": "COMMUNITY",
-	"wiki_url": "", # todo: insert github url
+    "name": "pcb2blender importer",
+    "description": "Enables Blender to import .pcb3d files, exported from KiCad.",
+    "author": "Bobbe",
+    "version": (1, 0, 0),
+    "blender": (3, 1, 0),
+    "location": "File > Import",
+    "category": "Import-Export",
+    "support": "COMMUNITY",
+    "wiki_url": "https://github.com/30350n/pcb2blender",
 }
 
 import bpy
@@ -17,15 +17,15 @@ module_names = ("import",)
 modules = []
 
 for module_name in module_names:
-	if module_name in locals():
-		modules.append(importlib.reload(locals()[module_name]))
-	else:
-		modules.append(importlib.import_module("." + module_name, package=__package__))
+    if module_name in locals():
+        modules.append(importlib.reload(locals()[module_name]))
+    else:
+        modules.append(importlib.import_module("." + module_name, package=__package__))
 
 def register():
-	for module in modules:
-		module.register()
+    for module in modules:
+        module.register()
 
 def unregister():
-	for module in modules:
-		module.unregister()
+    for module in modules:
+        module.unregister()
