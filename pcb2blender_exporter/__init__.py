3c2e73e2fd721973f3ff7aa026e4acf2702da02d
Bobbe <maxschlecht@web.de>
2023-04-25 21:46:58 +0200

    Bump addon version to 2.6, blender version to 3.5.1

--- pcb2blender_exporter/__init__.py
+++ pcb2blender_exporter/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from .ui import SettingsDialog
 from .export import export_pcb3d, get_boarddefs
 
-__version__ = "2.5"
+__version__ = "2.6"
 
 class Pcb2BlenderExporter(pcbnew.ActionPlugin):
     def defaults(self):
--- pcb2blender_importer/__init__.py
+++ pcb2blender_importer/__init__.py
@@ -2,8 +2,8 @@ bl_info = {
     "name": "pcb2blender importer",
     "description": "Enables Blender to import .pcb3d files, exported from KiCad.",
     "author": "Bobbe",
-    "version": (2, 5, 0),
-    "blender": (3, 5, 0),
+    "version": (2, 6, 0),
+    "blender": (3, 5, 1),
     "location": "File > Import",
     "category": "Import-Export",
     "support": "COMMUNITY",
@@ -11,7 +11,7 @@ bl_info = {
     "tracker_url": "https://github.com/30350n/pcb2blender/issues",
 }
 
-__version__ = "2.5"
+__version__ = "2.6"
 
 from .blender_addon_utils import add_dependencies, register_modules_factory
 
