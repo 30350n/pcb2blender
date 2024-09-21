c83000e09a4926188f90fa7ab292b993b621e262
Bobbe <maxschlecht@web.de>
2024-09-21 05:05:54 +0200

    Add blender_manifest for importer, remove bl_info, version

--- pcb2blender_importer/__init__.py
+++ pcb2blender_importer/__init__.py
@@ -1,19 +1,4 @@
-bl_info = {
-    "name": "pcb2blender importer",
-    "description": "Enables Blender to import .pcb3d files, exported from KiCad.",
-    "author": "Bobbe",
-    "version": (2, 12, 0),
-    "blender": (4, 2, 0),
-    "location": "File > Import",
-    "category": "Import-Export",
-    "support": "COMMUNITY",
-    "doc_url": "https://github.com/30350n/pcb2blender",
-    "tracker_url": "https://github.com/30350n/pcb2blender/issues",
-}
-
 import importlib
-
-__version__ = "2.12"
 
 MODULE_NAMES = ("importer", "materials", "solder_joints")
 _modules = []
