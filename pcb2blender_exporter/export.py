9d885de78fbdff0010a3346b26d607a909af8d3e
Heath Raftery <hraftery@gmail.com>
2024-11-07 02:18:37 +1100

    Save temp kicad_pcb file without saving project (#68)

--- pcb2blender_exporter/export.py
+++ pcb2blender_exporter/export.py
@@ -214,7 +214,7 @@ def get_stackup(board):
     stackup = Stackup()
 
     tmp_path = get_temppath("pcb2blender_tmp.kicad_pcb")
-    board.Save(str(tmp_path))
+    pcbnew.SaveBoard(str(tmp_path), board, aSkipSettings=True)
     content = tmp_path.read_text(encoding="utf-8")
 
     if not (match := stackup_regex.search(content)):
