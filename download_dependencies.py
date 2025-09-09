409a9a8d2600abb0a1668c0211d120d1a5e74589
Max Schlecht <maxschlecht@web.de>
2025-09-09 11:33:08 +0200

    Refactor download_dependencies, Support arm64 platforms (#105, #109)

--- download_dependencies.py
+++ download_dependencies.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python3
 
+import re
 import shutil
 from pathlib import Path
-from subprocess import DEVNULL, CalledProcessError, check_call
+from subprocess import STDOUT, CalledProcessError, check_output
+from typing import Any, cast
 
 import tomlkit
 from error_helper import warning
 
 FILE_DIR = Path(__file__).parent
+PYPROJECT_TOML = FILE_DIR / "pyproject.toml"
 BLENDER_ADDON_DIR = FILE_DIR / "pcb2blender_importer"
 BLENDER_ADDON_MANIFEST_TOML = BLENDER_ADDON_DIR / "blender_manifest.toml"
 BLENDER_ADDON_WHEEL_DIR = BLENDER_ADDON_DIR / "wheels"
 
-BLENDER_PYTHON_VERSION = "3.11"
-
-BLENDER_ADDON_PYTHON_DEPENDENCIES = [
-    "error-helper",
-    "skia-python",
-    "pillow",
-]
-
 BLENDER_ADDON_SUPPORTED_PLATFORMS = {
     "windows-x64": ("win_amd64",),
-    "linux-x64": ("manylinux_2_28_x86_64", "manylinux_2_17_x86_64"),
-    "macos-x64": ("macosx_10_10_x86_64", "macosx_10_9_x86_64"),
+    "windows-arm64": ("win_arm64",),
+    "linux-x64": ("manylinux_2_17_x86_64", "manylinux_2_28_x86_64"),
+    "linux-arm64": ("manylinux_2_17_aarch64", "manylinux_2_28_aarch64"),
+    "macos-x64": ("macosx_10_10_x86_64", "macosx_11_0_x86_64"),
     "macos-arm64": ("macosx_11_0_arm64",),
 }
 
 DOWNLOAD_BASE_COMMAND = (
     "pip",
     "download",
-    "--quiet",
     *("--dest", BLENDER_ADDON_WHEEL_DIR),
     *("--only-binary", ":all:"),
-    *("--python-version", BLENDER_PYTHON_VERSION),
 )
 
 
@@ -42,22 +37,34 @@ def download_blender_extension_dependencies():
         shutil.rmtree(BLENDER_ADDON_WHEEL_DIR)
     BLENDER_ADDON_WHEEL_DIR.mkdir()
 
-    for dependency in BLENDER_ADDON_PYTHON_DEPENDENCIES:
+    with PYPROJECT_TOML.open() as file:
+        pyproject = cast(dict[str, Any], tomlkit.load(file))
+        dependencies: list[str] = pyproject["project"]["dependencies"]
+        python_version: str = pyproject["project"]["requires-python"].strip("=.*")
+
+    for dependency in dependencies:
         for blender_platform, wheel_platforms in BLENDER_ADDON_SUPPORTED_PLATFORMS.items():
-            for wheel_platform in wheel_platforms:
-                try:
-                    command = DOWNLOAD_BASE_COMMAND + ("--platform", wheel_platform, dependency)
-                    check_call(command, stdout=DEVNULL, stderr=DEVNULL)
-                    break
-                except CalledProcessError:
-                    continue
-            else:
-                warning(f"failed to download '{dependency}' for '{blender_platform}'")
+            try:
+                output = check_output(
+                    DOWNLOAD_BASE_COMMAND
+                    + ("--python-version", python_version)
+                    + sum(zip(("--platform",) * len(wheel_platforms), wheel_platforms), ())
+                    + (dependency,),
+                    stderr=STDOUT,
+                )
+            except CalledProcessError as e:
+                warning(f"failed to download '{dependency}' for '{blender_platform}' with:")
+                warning(e.output.decode().replace("\n", "\n  "), prefix="  ")
+                continue
+
+            assert (match := re.search(r"^Saved (.*)\s*$", output.decode(), flags=re.MULTILINE))
+            if match.group(1).strip().endswith("-any.whl"):
+                break
 
     for numpy_wheel in BLENDER_ADDON_WHEEL_DIR.glob("numpy-*.whl"):
         numpy_wheel.unlink()
 
-    blender_manifest = tomlkit.parse(BLENDER_ADDON_MANIFEST_TOML.read_text())
+    blender_manifest = tomlkit.loads(BLENDER_ADDON_MANIFEST_TOML.read_text())
     blender_manifest["wheels"] = [
         f"./wheels/{filepath.name}" for filepath in BLENDER_ADDON_WHEEL_DIR.glob("*.whl")
     ]
